c963c0820cb743a5886226d31f190f4446e13dc9
Matthew Williams <mwilliamsnov6@gmail.com>
2015-05-21 21:44:20 -0700

    regEx version works for any number

--- script.js
+++ script.js
@@ -1,92 +1,99 @@
  $(document).ready(function(){
-var left = null;
-var operator = null;
-var right = null;
+// var left = null;
+// var operator = null;
+// var right = null;
 var total = [];
- 	$("#calculator").click(function(event) {
-	//$(this).css("backgroundColor","black")
-	
-	//event.target.style.backgroundColor="white"
-	//console.log($(event.target).text())
+ 	$("span").click(function(event) {
+
 	var buttonPressed = $(event.target).text()
-	//$("#screen").html(buttonPressed)
 	if (buttonPressed === "C"){
 		$("#screen").html("")
 		total = [];
 	}
 
-	if (buttonPressed !== "=" && buttonPressed !== "C") {
+
+	if (buttonPressed !== "=" && buttonPressed !== "C" 
+		&& buttonPressed !== String.fromCharCode(247)) {
 		$("#screen").append(buttonPressed)
 		total.push(buttonPressed)
 	}
-	if (buttonPressed === "=") {
-		//console.log(4444)
-		if (total[1] === "+") {
-			$("#screen").html("")
-			$("#screen").append(
-				 parseInt(total[0]) + parseInt(total[2]))
-		}
-		if (total[1] === "-") {
-			$("#screen").html("")
-			$("#screen").append(
-				 parseInt(total[0]) - parseInt(total[2]))
-		}
-		if (total[1] === "x") {
-			$("#screen").html("")
-			$("#screen").append(
-				 parseInt(total[0]) * parseInt(total[2]))
-		} else {
-			$("#screen").html("")
-			$("#screen").append(
-				 parseInt(total[0]) / parseInt(total[2]))
-		}	
-		//console.log("\&divide", "foooo");
 
-		// if (total[1] === "&divide;") {
-		// 	$("#screen").html("")
-		// 	$("#screen").append(
-		// 		 parseInt(total[0]) / parseInt(total[2]))
-		// }
-					
-
-
-	total = [];
+	if (buttonPressed === String.fromCharCode(247)) {
+		$("#screen").append(String.fromCharCode(247))
+		total.push("/")
 	}
+	// more complex version
+
+		if (buttonPressed === "=") {
+
+		total = total.join("")
+
+
+		var re = /[x+-\/]/
+		var match = total.match(re)
+		total = total.split(re)
+
+		console.log (total, "total")
+		console.log(match[0]);
+		operator = match[0]
+
+
+
+		if (match[0] === "+") {
+			$("#screen").html("")
+			$("#screen").append(
+				 parseInt(total[0]) + parseInt(total[1]))
+		}
+		if (match[0] === "-") {
+			$("#screen").html("")
+			$("#screen").append(
+				 parseInt(total[0]) - parseInt(total[1]))
+		}
+		if (match[0] === "x") {
+			$("#screen").html("")
+			$("#screen").append(
+				 parseInt(total[0]) * parseInt(total[1]))
+		}
+		if (match[0] === "/") {
+			$("#screen").html("")
+			$("#screen").append(
+				 parseInt(total[0]) / parseInt(total[1]))
+		}
+
+	}
+	// simple version
+
+	// if (buttonPressed === "=") {
+	// 	//console.log(4444)
+	// 	if (total[1] === "+") {
+	// 		$("#screen").html("")
+	// 		$("#screen").append(
+	// 			 parseInt(total[0]) + parseInt(total[2]))
+	// 	}
+	// 	if (total[1] === "-") {
+	// 		$("#screen").html("")
+	// 		$("#screen").append(
+	// 			 parseInt(total[0]) - parseInt(total[2]))
+	// 	}
+	// 	if (total[1] === "x") {
+	// 		$("#screen").html("")
+	// 		$("#screen").append(
+	// 			 parseInt(total[0]) * parseInt(total[2]))
+	// 	} else {
+	// 		$("#screen").html("")
+	// 		$("#screen").append(
+	// 			 parseInt(total[0]) / parseInt(total[2]))
+	// 	}	
+
+
+	// total = [];
+	// }
 	console.log(total)
 	
 
-	// if (total === null) {   //first entry
-	// 	store = buttonPressed
-	// 	total += 1
-	// }
-	// if (total.length === 1) {
-	// 	console.log('LEFT is', buttonPressed)
-	// 	left = buttonPressed
-	// 	total += 1
-	// }
-	// if (total.length === 1) {
-	// 		console.log('right is', buttonPressed)
-	// 	right = buttonPressed
-	// }
-
-	// if (buttonPressed === "+"){  // + operator
-
-	// }
-	// if (buttonPressed === "=") {
-
-	// 	$("#screen").html("dd")
-	// 	console.log("touched =")
-	// } 
-	// else {
-	// 	store += buttonPressed	
-	// }
-	//console.log (store);
-	//console.log (buttonPressed === NaN)
 	})
  	
- 	// $("calculator").on("click",function(){
- 	// 	console.log(this "was clicked") 
- 	// });
+
 
  })	
 
